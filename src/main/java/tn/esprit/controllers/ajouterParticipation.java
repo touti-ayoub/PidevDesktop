4023049cc62ecb6b6c19eb5697d2be5c41d0d5f1
Abdelmoamen hamemi <moamenhammami93@gmail.com>
2024-02-25 16:16:25 +0100

    aprés l ajout d'une Participation naviguer dans list Participation aprés la mise à jour des données

--- src/main/java/tn/esprit/controllers/ajouterParticipation.java
+++ src/main/java/tn/esprit/controllers/ajouterParticipation.java
@@ -1,8 +1,14 @@
 package tn.esprit.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
 import tn.esprit.models.Competition;
 import tn.esprit.models.Participation;
 import tn.esprit.models.Utilisateur;
@@ -11,6 +17,7 @@ import tn.esprit.services.ParticipationService;
 import tn.esprit.services.UtilisateurService;
 
 import java.awt.event.ActionEvent; // Cette importation semble inutile
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -47,7 +54,7 @@ public class ajouterParticipation {
     @FXML
 
 
-    public void ajouterParticipation(javafx.event.ActionEvent actionEvent) throws SQLException {
+    public void ajouterParticipation(javafx.event.ActionEvent actionEvent) throws SQLException, IOException {
             // Récupérer la compétition sélectionnée
             Competition competitionSelectionnee = listCompetition.getValue();
 
@@ -66,7 +73,27 @@ public class ajouterParticipation {
             } else {
                 // Créer et ajouter la participation
                 ps.ajouterParticipation(new Participation(competitionSelectionnee.getCodeC(), descriptionParticipation ,utilisateurSelectionne.getCodeU()));
+                // Après avoir ajouté la participation avec succès, naviguez vers la liste des participations
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/listeParticipation.fxml"));
+                Parent root = loader.load();
 
+                // Obtenez le contrôleur de la nouvelle interface et transmettez les données
+                listeParticipation controllerListeParticipation = loader.getController();
+
+                // Appelez la méthode de rafraîchissement sur l'instance de listeParticipation
+                controllerListeParticipation.refreshListeParticipations();
+
+                // Créez une nouvelle scène et configurez-la dans votre stage principal
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+
+                // Montrez la nouvelle scène
+                stage.show();
+
+                // Fermez la scène actuelle (ajouterParticipation)
+                Stage currentStage = (Stage) listCompetition.getScene().getWindow();
+                currentStage.close();
                 // Afficher un message de succès
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
                 alert.setTitle("Succès");
@@ -75,6 +102,7 @@ public class ajouterParticipation {
                 alert.show();
             }
         }
+
         }
 
 
