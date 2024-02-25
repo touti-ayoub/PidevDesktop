4cd109e905fb0c3fff7c830b1a61dc711f8c3652
ayoub <titan.ayoub28@gmail.com>
2024-02-26 00:27:15 +0100

    playing with css

--- src/main/java/tn/esprit/controllers/DeleteFood.java
+++ src/main/java/tn/esprit/controllers/DeleteFood.java
@@ -7,17 +7,18 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import tn.esprit.models.Food;
 import tn.esprit.services.FoodService;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 public class DeleteFood {
-    @FXML
-    private TextField idTF;
 
     @FXML
     void addFood(ActionEvent event) {
@@ -43,17 +44,17 @@ public class DeleteFood {
 
     @FXML
     void delete(ActionEvent event) {
-        FoodService foodService = new FoodService();
-        int id = Integer.parseInt(idTF.getText());
+        String selectedFoodName = foodComboBox.getValue();
+        try {
+            Food foodToDelete = foodService.filterByName(selectedFoodName).get(0);
 
-        Alert confirmationAlert = new Alert(Alert.AlertType.CONFIRMATION);
-        confirmationAlert.setTitle("Confirmation");
-        confirmationAlert.setContentText("Are you sure you want to delete this food item?");
+            Alert confirmationAlert = new Alert(Alert.AlertType.CONFIRMATION);
+            confirmationAlert.setTitle("Confirmation");
+            confirmationAlert.setContentText("Are you sure you want to delete " + selectedFoodName + "?");
 
-        Optional<ButtonType> result = confirmationAlert.showAndWait();
-        if (result.get() == ButtonType.OK){
-            try {
-                boolean isDeleted = foodService.supprimer(id);
+            Optional<ButtonType> result = confirmationAlert.showAndWait();
+            if (result.get() == ButtonType.OK){
+                boolean isDeleted = foodService.supprimer(foodToDelete.getId());
                 Alert alert;
                 if (isDeleted) {
                     alert = new Alert(Alert.AlertType.CONFIRMATION);
@@ -62,15 +63,22 @@ public class DeleteFood {
                 } else {
                     alert = new Alert(Alert.AlertType.INFORMATION);
                     alert.setTitle("INFO");
-                    alert.setContentText("Food ID not found in the database, no deletion was performed");
+                    alert.setContentText("Food name not found in the database, no deletion was performed");
                 }
                 alert.showAndWait();
-            } catch (SQLException e) {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("ERROR !!!");
-                alert.setContentText(e.getMessage());
-                alert.showAndWait();
+            } else {
+                // User chose CANCEL or closed the dialog
             }
+        } catch (SQLException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("ERROR !!!");
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
+        } catch (IndexOutOfBoundsException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("ERROR !!!");
+            alert.setContentText("Food name not found in the database, no deletion was performed");
+            alert.showAndWait();
         }
     }
 
@@ -143,7 +151,7 @@ public class DeleteFood {
     }
 
     public void recipeNavigate(ActionEvent actionEvent) {
-try {
+        try {
             // Load the FXML file
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/mainRecipe.fxml"));
 
@@ -160,6 +168,27 @@ try {
             stage.show();
         } catch (IOException e) {
             System.err.println("Failed to load the page: " + e.getMessage());
+        }
+    }
+
+    @FXML
+    private ComboBox<String> foodComboBox;
+
+    private FoodService foodService = new FoodService();
+
+    @FXML
+    public void initialize() {
+        try {
+            fillComboBox();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void fillComboBox() throws SQLException {
+        List<Food> foods = foodService.recuperer();
+        for (Food food : foods) {
+            foodComboBox.getItems().add(food.getName());
         }
     }
 }
