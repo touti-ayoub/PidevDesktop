ecdd1f73f75d3b2290fc2842f58177756f9f3bad
mootez <mootez.ouri@esprit.tn>
2024-02-27 01:28:08 +0100

    interface ajouter produit

--- src/main/java/controllers/Afficherproduit.java
+++ src/main/java/controllers/Afficherproduit.java
@@ -1,4 +1,47 @@
 package controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import tn.esprit.service.ProduitService;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class Afficherproduit {
+    @FXML
+    private TableColumn<produit,String> nomCol;
+
+    @FXML
+    private TableColumn<produit, Integer> prixCol;
+
+    @FXML
+    private TableColumn<produit, Integer> tailleCol;
+
+    @FXML
+    private TableColumn<produit, String> typeCol;
+
+    @FXML
+    private TableView<produit> tableView;
+
+    @FXML
+    void initialize() {
+        ProduitService produitService = new produitService();
+        try {
+            List<produit> produits = produitService.recuperer();
+            ObservableList<produit> observableList = FXCollections.observableList(produits);
+            tableView.setItems(observableList);
+
+            nomCol.setCellValueFactory(new PropertyValueFactory<>("nom"));
+            typeCol.setCellValueFactory(new PropertyValueFactory<>("type"));
+            tailleCol.setCellValueFactory(new PropertyValueFactory<>("taille"));
+            prixCol.setCellValueFactory(new PropertyValueFactory<>("prix"));
+        } catch (SQLException e) {
+          System.err.println(e.getMessage());
+        }
+
+    }
 }
---
+++ src/main/java/tn/esprit/test/mainFX.java
@@ -0,0 +1,16 @@
+package tn.esprit.test;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class mainFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
